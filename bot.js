cd9217215199a8c0d935c3f2c6ddafde9ce91287
Arnav Sirigere <arnavsirigere@gmail.com>
2020-05-24 11:22:15 +0530

    Spelling

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const apiKey = process.env.WORDNIK_API_KEY;
 const config = {
   consumer_key: process.env.CONSUMER_KEY,
   consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
+  access_token: process.env.ACCESS_TOKEN
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
 let T = new Twit(config);
