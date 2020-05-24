d0bea78b2127a8a8db1476e4c4933a1ae0b9b2a6
Arnav Sirigere <arnavsirigere@gmail.com>
2020-05-24 17:35:04 +0530

    Hitting the bot every 5 minutes to prevent sleeping

--- bot.js
+++ bot.js
@@ -1,5 +1,17 @@
 console.log('Word Bot starting!');
 
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get('/', (request, response) => {
+  console.log(Date.now() + ' Ping Received');
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
 require('dotenv').config();
 let fetch = require('node-fetch');
 let Twit = require('twit');
